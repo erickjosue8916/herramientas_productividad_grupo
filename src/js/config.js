d84e3ff244aa78fb28e7bc1ece16f4d98b0abec6
Erick Saravia <ericks@labs.aeegle.com>
2020-05-08 17:07:02 -0600

    definiendo estructura de cartas

--- src/js/config.js
+++ src/js/config.js
@@ -10,7 +10,9 @@ const config = {
   const information = [ 
     {
         title: "Misión",
-        body: `hdksdksdh`
+        body: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled`
     },
     {
         title: "Visión",
